1daa6c93ee39db7e2e1bbf9e1eb79459911932f1
Masahiro Hayashi <mhayashi1120@gmail.com>
2014-10-08 17:45:46 +0900

    fix

--- test.scm
+++ test.scm
@@ -3,15 +3,11 @@
 ;;;
 
 (use gauche.test)
+(use srfi-19)
 
 (test-start "rfc.3339")
 (use rfc.3339)
 (test-module 'rfc.3339)
-
-;; The following is a dummy test code.
-;; Replace it for your tests.
-(test* "test-rfc_3339" "rfc_3339 is working"
-       (test-rfc_3339))
 
 (define (date=? d1 d2)
   (let loop ([slots (class-slots <date>)])
@@ -25,12 +21,18 @@
 (define (date y m d H M S MS offset)
   (make-date (* MS 1000000) S M H d m y offset))
 
-;;TODO test
-(should (date=? (rfc3339-date->date "2014-01-02T03:04:05Z") (date 2014 1 2 3 4 5 0 0)))
-(should (date=? (rfc3339-date->date "2014-01-02T03:04:05+09:00") (date 2014 1 2 3 4 5 0 32400)))
-(should (date=? (rfc3339-date->date "2014-01-02T03:04:05-09:50") (date 2014 1 2 3 4 5 0 -35400)))
+(define (should expected result :optional (msg #f))
+  (test* (or msg (x->string expected))
+         expected result))
 
-(should (date=? (rfc3339-date->date "2014-01-02 03:04:05Z") (date 2014 1 2 3 4 5 0 0)))
+(define (date-should expected result :optional (msg #f))
+  (test* (or msg expected)
+         expected result date=?))
+
+(date-should (date 2014 1 2 3 4 5 0 0)      (rfc3339-date->date "2014-01-02T03:04:05Z"))
+(date-should (date 2014 1 2 3 4 5 0 32400)  (rfc3339-date->date "2014-01-02T03:04:05+09:00"))
+(date-should (date 2014 1 2 3 4 5 0 -35400) (rfc3339-date->date "2014-01-02T03:04:05-09:50"))
+(date-should (date 2014 1 2 3 4 5 0 0)      (rfc3339-date->date "2014-01-02 03:04:05Z"))
 
 ;; If you don't want `gosh' to exit with nonzero status even if
 ;; the test fails, pass #f to :exit-on-failure.
