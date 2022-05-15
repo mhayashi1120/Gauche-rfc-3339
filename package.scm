4e4f84adb6d207e42b7d9dddb46f03897928e770
Masahiro Hayashi <mhayashi1120@gmail.com>
2022-05-15 10:16:26 +0900

    upgrade to the newest gauche-package generated maintenance version up

--- package.scm
+++ package.scm
@@ -4,7 +4,7 @@
 
 (define-gauche-package "Gauche-rfc-3339"
   ;; 
-  :version "1.3.3"
+  :version "1.3.4"
 
   ;; Description of the package.  The first line is used as a short
   ;; summary.
@@ -15,13 +15,15 @@
   ;; Example:
   ;;     :require (("Gauche" (>= "0.9.5"))  ; requires Gauche 0.9.5 or later
   ;;               ("Gauche-gl" "0.6"))     ; and Gauche-gl 0.6
-  :require ()
+  :require (("Gauche" (>= "0.9.11-p1")))
 
   ;; List of providing modules
   ;; NB: This will be recognized >= Gauche 0.9.7.
   ;; Example:
   ;;      :providing-modules (util.algorithm1 util.algorithm1.option)
-  :providing-modules ()
+  :providing-modules (
+                      rfc.3339
+                      )
   
   ;; List name and contact info of authors.
   ;; e.g. ("Eva Lu Ator <eval@example.com>"
